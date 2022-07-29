9f263489f91a09a3b547c48b158ef14987e99bc5
Brendan Abolivier <babolivier@matrix.org>
2022-07-29 10:48:24 +0200

    Re-add invalidate pass-through, not sure how that went away

--- synapse/storage/databases/main/roommember.py
+++ synapse/storage/databases/main/roommember.py
@@ -637,7 +637,9 @@ class RoomMemberWorkerStore(EventsWorkerStore):
         If a remote user only returns rooms this server is currently
         participating in.
         """
-        rooms = await self.get_rooms_for_user_with_stream_ordering(user_id)
+        rooms = await self.get_rooms_for_user_with_stream_ordering(
+            user_id, on_invalidate=on_invalidate
+        )
         return frozenset(r.room_id for r in rooms)
 
     @cached(
